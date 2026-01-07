d9b7de79ea27a1a9643d2b32b138135c68bf59f1
Ryuu519 <adrian45.rusu@gmail.com>
2026-01-07 16:58:59 +0200

    Al doilea commit pentru script (update)

--- script.sh
+++ script.sh
@@ -1,12 +1,21 @@
-cat /etc/hosts | while read ip name rest
+#!/bin/bash
+
+verificare_ip_dns() {
+    local nume_host=$1
+    local ip_hosts=$2
+    local server_dns=$3
+
+    real_ip=$(nslookup "$nume_host" "$server_dns" 2>/dev/null | awk '/^Address: / { print $2 }' | tail -n 1)
+
+    if [[ -n "$real_ip" && "$real_ip" != "$ip_hosts" ]]; then
+        echo "Bogus IP for $nume_host in /etc/hosts! (DNS $server_dns zice $real_ip)"
+    fi
+}
+
+cat /etc/hosts | while read -r ip name rest
 do
-	[[ -z "$ip" || "$ip" =~ ^# ]] && continue
-
-	[[ -z "$name" ]] && continue
-
-	real_ip=$(nslookup "$name" 2>/dev/null | awk '/^Address: / { print $2 }' | tail -n 1)
-
-	if [[ -n "$real_ip" && "$real_ip" != "$ip" ]]; then
-		echo "Bogus IP for $name in /etc/hosts!"
-	fi
+    [[ -z "$ip" || "$ip" =~ ^# ]] && continue
+    [[ -z "$name" ]] && continue
+    
+    verificare_ip_dns "$name" "$ip" "8.8.8.8"
 done
